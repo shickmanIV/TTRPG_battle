afc4b386a9ac2431e6bd24c2c067b15bc258bfd5
shickmanIV <samuel.hickman@wsu.edu>
2024-05-16 14:04:50 -0700

    Created actor umbrella class

--- Game/Actor.py
+++ Game/Actor.py
@@ -1,4 +1,4 @@
-from Classes import AbilityType
+from Classes.Constants import AbilityType
 from Classes.CharacterClass import DnDClass as CharClass
 
 
@@ -12,9 +12,19 @@ class Actor():
     # Default constructor
     def __init__(self):
         self.name = ""
+        self.charClass = None
+        self.level = 1
         self.abilityScores = {AbilityType.STR: 10, AbilityType.DEX: 10, 
                               AbilityType.CON: 10, AbilityType.INT: 10, 
                               AbilityType.WIS: 10, AbilityType.CHA: 10}
+        
+    def __str__(self):
+        return f"""
+        name: {self.name}
+        charClass: TODO: print charClass
+        level: {self.level}
+        abilityscores {self.abilityScores}
+        """
     
     # Get the status of the actor, including:
     # - Spell slots, total and remaining
