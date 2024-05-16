338f09027477fa312963138b13d684d956a18a46
shickmanIV <samuel.hickman@wsu.edu>
2024-05-16 14:03:19 -0700

    Added umbrella class for conditions

--- Classes/Conditions.py
+++ Classes/Conditions.py
@@ -1,9 +1,22 @@
 from abc import ABC, abstractmethod
+from Game.Actor import Actor
+import logging # Run with "--log=INFO"
 
 class Condition():
     def __init__(self, name: str, desc: str, effects=None):
         self.name = name
         self.desc = desc
         self.effects = effects or []
+
+    # Adds an effect by passing a lambda expression to the 
+    def addEffect(self, newEffect=None):
+        self.effects.append(newEffect)
+
+    def apply(self, target: Actor):
+        logging.info(f"Applying Condition {self.name} to Actor {target.name}")
+        for modify in self.effects:
+            modify(target)
+        return
+    
 
         
