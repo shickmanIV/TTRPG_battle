faa2630a2d60c46a1debb7ca94e0bd427d0ead37
shickmanIV <samuel.hickman@wsu.edu>
2024-05-20 13:32:30 -0700

    Fixed logging config

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import test
+import logging
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
     test.test3()
--- systems/health_system.py
+++ systems/health_system.py
@@ -4,14 +4,17 @@ import logging # Run with "--log=INFO"
 
 # Really just an example for reference
 class HealthSystem(System):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
     def update(self, entity_manager):
         for entity in entity_manager.get_all_entities_with_component(HealthComponent):
             health_component = entity.get_component(HealthComponent)
             # Perform health-related updates
             if self.instant_death_check(health_component):
-                logging.info(f"Entity {entity.entity_id} dies instantly.")
+                self.logger.info(f"Entity {entity.entity_id} dies instantly.")
             elif health_component.current_hp == 0:
-                logging.info(f"Entity {entity.entity_id} is unconcious.")
+                self.logger.info(f"Entity {entity.entity_id} is unconcious.")
 
     def instant_death_check(self, health_component: HealthComponent):
         if health_component.current_hp < 0:
