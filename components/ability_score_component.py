8c9886920d95bd0c2d29505dfe215f1429dceddf
shickmanIV <samuel.hickman@wsu.edu>
2024-05-21 17:06:30 -0700

    Added AbilityScores component

---
+++ components/ability_score_component.py
@@ -0,0 +1,29 @@
+from typing import Any
+from components.component import Component
+
+class AbilityScores(Component):
+    def __init__(self, **args):
+        self.scores = {'str': args.get('str', 10),
+                       'dex': args.get('dex', 10),
+                       'con': args.get('con', 10),
+                       'int': args.get('int', 10),
+                       'wis': args.get('wis', 10),
+                       'cha': args.get('cha', 10)}
+        
+    def bonus(self, abilitytype) -> int:
+        value = self.scores.get(abilitytype)
+        return int((value - 10) / 2)
+    
+    def print_attributes(self, indent=""):
+        for key, value in self.scores.items():
+            bonus = self.bonus(key)
+            print(f"{indent}{key}: {value}\t({'+' if (bonus >= 0) else ''}{bonus})")
+
+
+
+        
+        
+
+
+    
+
--- presets/characters.py
+++ presets/characters.py
@@ -3,5 +3,5 @@ from entities.entity import Entity
 from entities.actor import Actor
 
 def grog() -> Entity:
-    grog = Actor('grog', name="Grog", max_hp=12)
+    grog = Actor('grog', name="Grog", max_hp=12, str=14, con=16, dex=9, int=6, wis=11, cha=9)
     return grog
