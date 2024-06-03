3aec11e453dc7708167bfcdf094a6e046059c193
shickmanIV <samuel.hickman@wsu.edu>
2024-06-03 15:28:37 -0700

    Added simple documentation via docstrings

--- event.py
+++ event.py
@@ -1,4 +1,12 @@
 class Event:
+    """
+    Represents an event in a TTRPG battle.
+
+    Attributes:
+        type (str): The type of the event.
+        data (Any): Additional data associated with the event.
+    """
+
     def __init__(self, type, data=None):
         self.type = type
         self.data = data
--- event_bus.py
+++ event_bus.py
@@ -1,12 +1,29 @@
 class EventBus:
+    """
+    A simple event bus implementation that allows subscribing to and publishing events.
+    """
+
     def __init__(self):
         self.subscribers = {}
 
     def subscribe(self, event_type, system):
+        """
+        Subscribe a system to a specific event type.
+
+        Args:
+            event_type (str): The type of event to subscribe to.
+            system (object): The system object that will handle the event.
+        """
         if event_type not in self.subscribers:
             self.subscribers[event_type] = []
         self.subscribers[event_type].append(system)
 
     def publish(self, event):
+        """
+        Publish an event to all subscribed systems.
+
+        Args:
+            event (object): The event object to publish.
+        """
         for system in self.subscribers.get(event.type, []):
             system.handle_event(event)
