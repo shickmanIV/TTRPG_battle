147fbdcadee7e43f9fbe0163c6ba0abfb79d4f49
shickmanIV <samuel.hickman@wsu.edu>
2024-06-03 14:29:26 -0700

    Modified HealthSystem to publish 'entity_unconscious' and 'entity_death' events to an EventBus

--- systems/health_system.py
+++ systems/health_system.py
@@ -2,18 +2,22 @@ from systems.system import System
 from components.health_component import HealthComponent
 import logging # Run with "--log=INFO"
 
+logger = logging.getLogger(__name__)
+
 class HealthSystem(System):
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
+    def __init__(self, event_bus):
+        self.event_bus = event_bus
 
     def update(self, entity_manager):
         for entity in entity_manager.get_all_entities_with_component(HealthComponent):
             health_component = entity.get_component(HealthComponent)
             # Perform health-related updates
             if self.instant_death_check(health_component):
-                self.logger.info(f"Entity {entity.entity_id} dies instantly.")
+                logger.info(f"Entity {entity.entity_id} dies instantly.")
+                self.event_bus.publish({"type": "entity_death", "entity": entity})
             elif health_component.current_hp == 0:
-                self.logger.info(f"Entity {entity.entity_id} is unconcious.")
+                logger.info(f"Entity {entity.entity_id} is unconscious.")
+                self.event_bus.publish({"type": "entity_unconscious", "entity": entity})
 
     def instant_death_check(self, health_component: HealthComponent):
         if health_component.current_hp < 0:
