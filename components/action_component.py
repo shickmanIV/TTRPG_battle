af0585dd9d554be65acdaf480f1235326e7da2bd
shickmanIV <samuel.hickman@wsu.edu>
2024-06-16 14:17:38 -0700

    Added basic documentation to components

--- components/action_component.py
+++ components/action_component.py
@@ -5,6 +5,14 @@ valid_types = {'ACTION', 'BONUS', 'REACTION', 'FREE'}
 
 # A class for representing a specific action taken by a player
 class ActionComponent:
+    """
+    Represents a specific action taken by a player.
+
+    Attributes:
+        action_type (str): The type of action.
+        target: The target of the action. Defaults to None.
+    """
+
     def __init__(self, action_type, target=None):
         if action_type not in valid_types:
             logger.warning(f"Invalid Action Type: '{action_type}'")
--- components/health_component.py
+++ components/health_component.py
@@ -1,13 +1,27 @@
 from components.component import Component
 
-# Really just an example for reference
 class HealthComponent(Component):
+    """
+    A class representing the health component of a character.
+
+    Attributes:
+        current_hp (int): The current hit points of the character.
+        max_hp (int): The maximum hit points of the character.
+        temp_hp (int): Temporary hit points that can be added to the character's current hit points.
+    """
+
     def __init__(self, max_hp):
         self.current_hp = max_hp
         self.max_hp = max_hp
         self.temp_hp = 0
 
     def print_attributes(self, indent=""):
+        """
+        Prints the attributes of the HealthComponent.
+
+        Args:
+            indent (str, optional): The starting indentation amount to be used for printing. Defaults to "".
+        """
         print(indent + "current_hp:", self.current_hp)
         print(indent + "max_hp:", self.max_hp)
         print(indent + "temp_hp:", self.temp_hp)
